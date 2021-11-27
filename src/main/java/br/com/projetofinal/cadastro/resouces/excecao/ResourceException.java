22ddbcb1f2d472d162a632c6e47d86a4ee57d573
AdrianaAmaral <adrianafsamaral@gmail.com>
2021-11-27 14:13:37 -0300

    ProjetoFinalCast

--- src/main/java/br/com/projetofinal/cadastro/resouces/excecao/ResourceException.java
+++ src/main/java/br/com/projetofinal/cadastro/resouces/excecao/ResourceException.java
@@ -15,38 +15,38 @@ import br.com.projetofinal.cadastro.services.excecao.DataIntegrityViolationExcep
 @ControllerAdvice
 public class ResourceException {
 
-//	@ExceptionHandler(ObjectnotFoundException.class)
-//	public ResponseEntity<StandardError> ObjetoExcecao(ObjectnotFoundException ex,
-//			HttpServletRequest request) {
+	@ExceptionHandler(ObjectnotFoundException.class)
+	public ResponseEntity<StandardError> ObjetoExcecao(ObjectnotFoundException ex,
+			HttpServletRequest request) {
 
-//		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
-//				"Object Not Found", ex.getMessage(), request.getRequestURI());
-//
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-//	}
-//	
-//	@ExceptionHandler(DataIntegrityViolationException.class)
-//	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
-//			HttpServletRequest request) {
-//
-//		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
-//				"Violação de Dados", ex.getMessage(), request.getRequestURI());
-//
-//		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-//	}
-//	
-//	@ExceptionHandler(MethodArgumentNotValidException.class)
-//	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex,
-//			HttpServletRequest request) {
-//
-//		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
-//				"Validation error", "Erro na validação dos campos", request.getRequestURI());
-//		
-//		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
-//			errors.addError(x.getField(), x.getDefaultMessage());
-//		}
-//
-//		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-//	}
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Object Not Found", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de Dados", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
+
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Validation error", "Erro na validação dos campos", request.getRequestURI());
+		
+		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+	}
 }
-//
+
